3fe93331aaaad698eb0c0d35aac2f2db22630a78
Patrick LaRocque <plarocque@mitre.org>
2023-02-22 17:26:31 -0500

    Fix final lint error.

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -17,7 +17,9 @@ program
   .option('-d --depends <path>', 'Path to the CQL dependency files.')
   .parse(process.argv);
 
-const { file, output, url, id, lib, depends } = program;
+const {
+  file, output, url, id, lib, depends
+ } = program;
 
 function getCqlFiles() {
   try {
